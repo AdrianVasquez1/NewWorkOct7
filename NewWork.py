726fac1accec02761ce624b3e44935ccb788af8b
AdrianVasquez1 <100114395@lubbockisd.net>
2019-10-07 08:49:17 -0500

    Adding descriptions to the strings

--- NewWork.py
+++ NewWork.py
@@ -1,3 +1,4 @@
+# There is a comparison in between a string that describes the number of people
 x = "There are %d types of people." % 10
 binary = "binary"
 doNot = "don't"
@@ -6,13 +7,16 @@ y = "Those who know %s and those who %s." % (binary, doNot)
 print(x)
 print(y)
 
+#  There is a string within a string creating one sentence
 print("I dsof: %r" % x)
 print("I also said: '%s'." % y)
 
+# A string evaluation is taking place comparing the joke to whether or not it is funny
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 print(joke_evaluation % hilarious)
 
+# There is two string creating one whole line when they are added together
 w = "This is the left side of ..."
 e = "a string with a right side"
 
